1ba7a76b3badfbce956dc9f555cffd5c022b15b9
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-10-14 18:29:29 +0800

    modified to include selective repeat protocol

--- lab3/util.py
+++ lab3/util.py
@@ -70,13 +70,16 @@ def pkt_to_string(pkt):
 
 
 def get_transport_layer_by_name(name, local_port, remote_port, msg_handler):
-  assert name == 'dummy' or name == 'ss' or name == 'gbn'
+  assert name == 'dummy' or name == 'ss' or name == 'gbn' or name == 'sr'
   if name == 'dummy':
     return dummy.DummyTransportLayer(local_port, remote_port, msg_handler)
   if name == 'ss':
     return ss.StopAndWait(local_port, remote_port, msg_handler)
   if name == 'gbn':
     return gbn.GoBackN(local_port, remote_port, msg_handler)
+  #TODO: modify this part
+  if name == 'sr':
+    pass
 
 
 def now():
